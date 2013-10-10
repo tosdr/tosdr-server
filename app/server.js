70b9031e42455c2ae77a93bd069b73c8c72e9234
Vincent Tunru <projects@vinnl.nl>
2013-10-10 15:10:52 +0200

    Allow the configuration to be loaded as a module.

--- app/server.js
+++ app/server.js
@@ -5,16 +5,19 @@ var express = require('express');
 var persona = require('express-persona')
 
 var app = express();
-var config = JSON.parse(fs.readFileSync('./package.json'));
+var config = require('config');
 
 if(config['tosdr-files']){
 	console.info('Serving files from', path.resolve(__dirname, '../', config['tosdr-files']));
 	app.use(express.static(path.resolve(__dirname, '../', config['tosdr-files'])));
 }
 
-app.use(express.cookieParser())
+app.use(express.bodyParser())
+   .use(express.cookieParser())
    .use(express.session({ secret: 'tosdrserver' }));
 persona(app, config.persona);
+
+app.use('/post/comment', require('saveComments'));
 
 app.listen(8000);
 console.info('Listening at port 8000...');

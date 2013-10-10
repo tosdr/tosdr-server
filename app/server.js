bce7728fcf22c53e21e821886609db265c0ddc31
Vincent Tunru <projects@vinnl.nl>
2013-10-10 12:27:21 +0200

    Verify Persona authentication.

--- app/server.js
+++ app/server.js
@@ -2,12 +2,19 @@
 
 var fs = require('fs'), path = require('path');
 var express = require('express');
+var persona = require('express-persona')
+
 var app = express();
 var config = JSON.parse(fs.readFileSync('./package.json'));
 
 if(config['tosdr-files']){
+	console.info('Serving files from', path.resolve(__dirname, '../', config['tosdr-files']));
 	app.use(express.static(path.resolve(__dirname, '../', config['tosdr-files'])));
 }
+
+app.use(express.cookieParser())
+   .use(express.session({ secret: 'tosdrserver' }));
+persona(app, config.persona);
 
 app.listen(8000);
 console.info('Listening at port 8000...');

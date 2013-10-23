9f61a4d812f8123144854b8d95f2902f1a192185
Vincent Tunru <projects@vinnl.nl>
2013-10-23 14:27:08 +0200

    Allow for cross-origin requests.

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@
 var fs = require('fs'), path = require('path');
 var express = require('express');
 var persona = require('express-persona');
+var cors = require('cors');
 
 var app = express();
 var config = require('config');
@@ -14,7 +15,8 @@ if(config['tosdr-files']){
 
 app.use(express.bodyParser())
    .use(express.cookieParser())
-   .use(express.session({ secret: 'tosdrserver' }));
+   .use(express.session({ secret: 'tosdrserver' }))
+   .use(cors({origin: config.persona.audience, credentials: true}));
 persona(app, config.persona);
 
 app.use('/post/comment', require('saveComments'));

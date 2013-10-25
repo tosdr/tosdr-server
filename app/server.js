3197e18113d781061d15f400d7bb2568a8d5386d
Vincent Tunru <projects@vinnl.nl>
2013-10-25 15:53:12 +0200

    Add ability to return a list of available services.

--- app/server.js
+++ app/server.js
@@ -7,10 +7,9 @@ var cors = require('cors');
 
 var app = express();
 var config = require('config');
-
-if(config['tosdr-files']){
-	console.info('Serving files from', path.resolve(__dirname, '../', config['tosdr-files']));
-	app.use(express.static(path.resolve(__dirname, '../', config['tosdr-files'])));
+if(config['tosdr-files'] && config['tosdr-files'].serve && config['tosdr-files'].location){
+	console.info('Serving files from', path.resolve(__dirname, '../', config['tosdr-files'].location));
+	app.use(express.static(path.resolve(__dirname, '../', config['tosdr-files'].location)));
 }
 
 app.use(express.bodyParser())
@@ -20,6 +19,7 @@ app.use(express.bodyParser())
 persona(app, config.persona);
 
 app.use('/post/comment', require('saveComments'));
+app.use('/get/services', require('getServices'));
 
 app.listen(8000);
 console.info('Listening at port 8000...');
